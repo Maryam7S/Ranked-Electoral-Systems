b5f922ee8a5b6ae52278293692a646c2368f5713
Maryam <maryam.amri@outlook.com>
2018-08-12 12:28:47 -0400

    easier to follow, removed hash table

--- kemeny-young.py
+++ kemeny-young.py
@@ -10,9 +10,8 @@ import random
 import itertools
 #################################################
 #DEFINE function to process ballots
-def reduce_votes(votes, can_list):
+def reduce_votes(votes, N_candidates):
 
-    N_candidates = len(can_list)
     pairwise_ballot=zeros([N_candidates, N_candidates]) #rows represent runners, columns represent opponents
     for ballot in votes:
         for a in range(N_candidates):
@@ -20,60 +19,64 @@ def reduce_votes(votes, can_list):
                 if a != b:
                     if ballot[a]>ballot[b]:
                         pairwise_ballot[a,b] +=1
-    return(pairwise_ballot, can_list, N_candidates) 
+    return(pairwise_ballot, N_candidates) 
 
 #################################################
 #DEFINE function to determine Kemeny-Young ranking
-def Kemeny_Young(pairwise_ballot, can_list, N_candidates):
+def Kemeny_Young(pairwise_ballot, N_candidates, candidate_order):
 
     #score all possible rankings
-    all_rankings=list(itertools.permutations(can_list))
-    print(all_rankings)
-    
+    all_rankings=list(itertools.permutations(range(N_candidates)))
     score_table = []
-    for ranking in all_rankings:  #ex [' Bruno', 'Cardi', 'Rocky']
+    
+    for ranking in all_rankings:  #ex [1,0,2] which represents [ 'Cardi', ' Bruno', 'Rocky']
         score=0
-        ranking_indices=[]
-        
-        for a in ranking: #ex 'Bruno'
-            ranking_indices.append(can_list.get(a))
-        for i in range(N_candidates):
-            b=i
-            while b < N_candidates-1:
-                b += 1
-                score = int(score+pairwise_ballot[ranking_indices[i], ranking_indices[b]]) #add number of voters who preferred i over b
+        for runner_index in range(N_candidates):  #ex position 0, which holds candidate 1 
+            opponent_index = runner_index
+            while opponent_index < N_candidates-1:
+                opponent_index += 1
+                runner = ranking[runner_index]
+                opponent = ranking[opponent_index]
+                score += int(pairwise_ballot[runner, opponent]) #add number of voters who preferred runner over opponent, retrieved from summed ballot
         score_table.append(score)
-    print(score_table)
     
 #decide winner
-    Max = int(max(score_table))
     final_ranking = []
-    for m in range(len(score_table)):
-        if score_table[m] == Max:
-            final_ranking.append(all_rankings[m]) 
+    for score_index in range(len(score_table)):
+        if score_table[score_index] == int(max(score_table)):
+            final_ranking.append(all_rankings[score_index]) 
 
+#interpret results
+    final_ranking_names = []
+    
     if len(final_ranking) > 1:
         print('There is a tie')
+        
+    for ranking in final_ranking:
+        each_final_ranking_names = []
+        for i in ranking:
+            each_final_ranking_names.append(candidate_order[i])
+        final_ranking_names.append(each_final_ranking_names)
     
-    print('Final ranking(s)', final_ranking)
-    return(final_ranking)
+    print('Final ranking(s) : ', final_ranking_names)
+    return(final_ranking_names)
 
 ###########################################################################
 ###########################################################################
 #imagine that an organization is voting on a candidate to fill a vacancy on its board of directors
-#the candidates' names are Bruno, Cardi, Rocky, Travis, Donald
-can_list = {'Bruno': 0, 'Cardi': 1, 'Rocky': 2}   #replace with dict
-N_candidates = len(can_list)
+#the candidates' names are Bruno, Cardi, Rocky
+candidate_order = ('Bruno', 'Cardi', 'Rocky') 
+N_candidates = len(candidate_order)
 
 #assign random votes
 N_voters = 10 #number of voters
 votes = zeros([N_voters, N_candidates])
-ballot = zeros(N_candidates)
+ballot = zeros(N_candidates)   
 for i in range(N_voters):
     for a in range (N_candidates):
         ballot[a]= random.randint(1, 5) 
     votes[i]=ballot
     
 #####################################################
-x=reduce_votes(votes, can_list)
-Kemeny_Young(x[0], x[1], x[2])
+x=reduce_votes(votes, len(candidate_order))
+Kemeny_Young(x[0], x[1], candidate_order)
